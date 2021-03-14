c9215914a8ecf815ceab4c6918520e8e2e197427
jiyeong1004 <ak1157@naver.com>
2021-03-14 18:22:49 +0900

    PART2(초급) - 오늘부터 나는 Java 프로그래머!

---
+++ src/kr/co/infopub/chapter/s014/Hello.java
@@ -0,0 +1,12 @@
+// 자바 프로그래밍 실행하기
+package kr.co.infopub.chapter.s014;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**********************************");
+		System.out.println("*  이 프로그래밍은 Java200이 만들었습니다.  *");
+		System.out.println("**********************************");
+	}
+}
---
+++ src/kr/co/infopub/chapter/s015/JavaPrimaryType.java
@@ -0,0 +1,16 @@
+// 기본 타입 선언하기
+package kr.co.infopub.chapter.s015;
+
+// 기본 타입의 종류
+public class JavaPrimaryType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isL = false;
+		char suit = 'H';
+		int year = 2016;
+		double latitude = 37.52127220511242;
+		double longitude = 127.0074462890635;
+		System.out.println(latitude + "\t" + longitude);
+	}
+}
