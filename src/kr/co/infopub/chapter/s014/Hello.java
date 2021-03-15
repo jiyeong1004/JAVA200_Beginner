ad0e0bd833e0132eaee316d3be143911386a185b
jiyeong1004 <ak1157@naver.com>
2021-03-16 00:00:44 +0900

    014. 자바 프로그래밍 실행하기

--- src/kr/co/infopub/chapter/s014/Hello.java
+++ src/kr/co/infopub/chapter/s014/Hello.java
@@ -5,8 +5,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("**********************************");
-		System.out.println("*  이 프로그래밍은 Java200이 만들었습니다.  *");
-		System.out.println("**********************************");
+		System.out.println("********************************");
+		System.out.println("* 이 프로그래밍은 Java200이 만들었습니다. *");
+		System.out.println("********************************");
 	}
 }
