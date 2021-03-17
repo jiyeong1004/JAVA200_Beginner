a0f371a1754a5d3e34360c8c366012b452751735
jiyeong1004 <ak1157@naver.com>
2021-03-18 00:47:26 +0900

    024. 메서드 선언하고 호출하기

---
+++ src/kr/co/infopub/chapter/s024/Hello.java
@@ -0,0 +1,18 @@
+// 024. 메서드 선언하고 호출하기
+package kr.co.infopub.chapter.s024;
+
+// 메서드
+public class Hello {
+	// 메서드 선언
+	public static void showHello() {
+		System.out.println("********************************");
+		System.out.println("* 이 프로그래밍은 Java200이 만들었습니다. *");
+		System.out.println("********************************");
+	}
+	
+	// 메서드 호출
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showHello();  // '메서드 이름()'
+	}
+}
