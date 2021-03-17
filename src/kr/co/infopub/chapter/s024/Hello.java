0549dfc755156e799ce9d5fc74be8ba0f50d944b
jiyeong1004 <ak1157@naver.com>
2021-03-18 00:49:40 +0900

    024. 메서드 선언하고 호출하기

--- src/kr/co/infopub/chapter/s024/Hello.java
+++ src/kr/co/infopub/chapter/s024/Hello.java
@@ -5,6 +5,10 @@ package kr.co.infopub.chapter.s024;
 public class Hello {
 	// 메서드 선언
 	public static void showHello() {
+	/*
+	 * 책에는 public static void showHelp() { 라고 적혀있는데
+	 * 아마 public static void showHello() { 를 오타낸듯
+	 */
 		System.out.println("********************************");
 		System.out.println("* 이 프로그래밍은 Java200이 만들었습니다. *");
 		System.out.println("********************************");
